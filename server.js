1cc190d98d9d953cc0f5edb275efc2ee5e8767f1
Glitch (hello-express) <none>
2020-09-14 17:13:48 +0000

    ☕️🍭 Checkpoint ./server.js:297617/439 ./views/index.html:297617/31716 ./public/style.css:297617/4833

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ var moment = require('moment');
 var plaid = require('plaid');
 
 var APP_PORT = envvar.number('APP_PORT', 8000);
-var PLAID_CLIENT_ID = envvar.string('PLAID_CLIENT_ID');
-var PLAID_SECRET = envvar.string('PLAID_SECRET');
-var PLAID_ENV = envvar.string('PLAID_ENV', 'sandbox');
+var PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
+var PLAID_SECRET = process.env.PLAID_SECRET;
+var PLAID_ENV = process.env.PLAID_ENV;
 // PLAID_PRODUCTS is a comma-separated list of products to use when initializing
 // Link. Note that this list must contain 'assets' in order for the app to be
 // able to create and retrieve asset reports.
-var PLAID_PRODUCTS = envvar.string('PLAID_PRODUCTS', 'transactions').split(',');
+var PLAID_PRODUCTS = process.env.PLAID_PRODUCTS.split(',');
 
 // PLAID_PRODUCTS is a comma-separated list of countries for which users
 // will be able to select institutions from.
-var PLAID_COUNTRY_CODES = envvar.string('PLAID_COUNTRY_CODES', 'US').split(',');
+var PLAID_COUNTRY_CODES = process.env.PLAID_COUNTY_CODES.split(',');
 
 // Parameters used for the OAuth redirect Link flow.
 //
