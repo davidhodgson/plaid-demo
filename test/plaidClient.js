1b72ec40f3d5291e29201c5090484b301ec2ad1f
Glitch (hello-express) <none>
2020-09-15 19:04:49 +0000

    💼🔮 Checkpoint ./test/plaidClient.js:297617/472

--- test/plaidClient.js
+++ test/plaidClient.js
@@ -1,10 +1,16 @@
 var expect = require('chai').expect;
-const PlaidClient = require("./plaidClient");
+const PlaidClient = require("../plaidClient");
 
-describe("test", function() {
+describe("PlaidClient", function() {
   
-  it("should run a test", function() {
-    expect(true).to.be.true;
-  })
+  it("should create a new PlaidClient", function() {
+    const plaidClient = new PlaidClient();
+    expect(plaidClient).to.not.be.null;
+  });
+  
+  it("should test createLinkToken", function() {
+    const plaidClient = new PlaidClient();
+    let linkToken = plaidClient.createLinkToken({});
+  });
   
 });
