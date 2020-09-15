b06f19750378fd130399fb16b2e6ddf40e5cc761
Glitch (hello-express) <none>
2020-09-15 17:20:19 +0000

    🎮🌊 Checkpoint ./server.js:297617/5532

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ var PLAID_REDIRECT_URI = envvar.string("PLAID_REDIRECT_URI", "");
 // We store the access_token in memory - in production, store it in a secure
 // persistent data store
 var ACCESS_TOKEN = null;
-var PUBLIC_TOKEN = null;
 var ITEM_ID = null;
 // The payment_id is only relevant for the UK Payment Initiation product.
 // We store the payment_id in memory - in production, store it in a secure
@@ -111,95 +110,16 @@ app.post("/api/create_link_token", function(request, response, next) {
         error: error
       });
     }
-    console.log("createTokenResponse: ", createTokenResponse);
     response.json(createTokenResponse);
   });
-});
-
-// Create a link token with configs which we can then use to initialize Plaid Link client-side.
-// See https://plaid.com/docs/#payment-initiation-create-link-token-request
-app.post("/api/create_link_token_for_payment", function(
-  request,
-  response,
-  next
-) {
-  client.createPaymentRecipient(
-    "Harry Potter",
-    "GB33BUKB20201555555555",
-    {
-      street: ["4 Privet Drive"],
-      city: "Little Whinging",
-      postal_code: "11111",
-      country: "GB"
-    },
-    function(error, createRecipientResponse) {
-      var recipientId = createRecipientResponse.recipient_id;
-
-      client.createPayment(
-        recipientId,
-        "payment_ref",
-        {
-          value: 12.34,
-          currency: "GBP"
-        },
-        function(error, createPaymentResponse) {
-          prettyPrintResponse(createPaymentResponse);
-          var paymentId = createPaymentResponse.payment_id;
-          PAYMENT_ID = paymentId;
-          const configs = {
-            user: {
-              // This should correspond to a unique id for the current user.
-              client_user_id: "user-id"
-            },
-            client_name: "Plaid Quickstart",
-            products: PLAID_PRODUCTS,
-            country_codes: PLAID_COUNTRY_CODES,
-            language: "en",
-            payment_initiation: {
-              payment_id: paymentId
-            }
-          };
-          if (PLAID_REDIRECT_URI !== "") {
-            configs.redirect_uri = PLAID_REDIRECT_URI;
-          }
-          client.createLinkToken(
-            {
-              user: {
-                // This should correspond to a unique id for the current user.
-                client_user_id: "user-id"
-              },
-              client_name: "Plaid Quickstart",
-              products: PLAID_PRODUCTS,
-              country_codes: PLAID_COUNTRY_CODES,
-              language: "en",
-              redirect_uri: PLAID_REDIRECT_URI,
-              payment_initiation: {
-                payment_id: paymentId
-              }
-            },
-            function(error, createTokenResponse) {
-              if (error != null) {
-                prettyPrintResponse(error);
-                return response.json({
-                  error: error
-                });
-              }
-              response.json(createTokenResponse);
-            }
-          );
-        }
-      );
-    }
-  );
 });
 
 // Exchange token flow - exchange a Link public_token for
 // an API access_token
 // https://plaid.com/docs/#exchange-token-flow
-app.post("/api/set_access_token", function(request, response, next) {
+app.post("/api/set_access_token", async function(request, response, next) {
   console.log("setting access token: ", request.body.public_token);
-  PUBLIC_TOKEN = request.body.public_token;
-  client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
+  let resp = await client.exchangePublicToken(request.body.public_token, function(error, tokenResponse){
     if (error != null) {
       prettyPrintResponse(error);
       return response.json({
@@ -221,13 +141,40 @@ app.post("/api/set_access_token", function(request, response, next) {
   });
 });
 
+/*
+// Exchange token flow - exchange a Link public_token for
+// an API access_token
+// https://plaid.com/docs/#exchange-token-flow
+app.post("/api/set_access_token", async function(request, response, next) {
+  console.log("setting access token: ", request.body.public_token);
+  client.exchangePublicToken(request.body.public_token, function(error, tokenResponse) {
+    if (error != null) {
+      prettyPrintResponse(error);
+      return response.json({
+        error: error
+      });
+    }
+    ACCESS_TOKEN = tokenResponse.access_token;
+    ITEM_ID = tokenResponse.item_id;
+    prettyPrintResponse(tokenResponse);
+
+    console.log("access token: ", ACCESS_TOKEN);
+    console.log("item id: ", ITEM_ID);
+
+    response.json({
+      access_token: ACCESS_TOKEN,
+      item_id: ITEM_ID,
+      error: null
+    });
+  });
+});
+*/
+
 // Retrieve an Item's accounts
 // https://plaid.com/docs/#accounts
 app.get("/api/accounts", async function(request, response, next) {
   let accessTokens = JSON.parse(request.query.data);
-  console.log("accessTokens: ", accessTokens);
   let access_tokens = accessTokens.access_tokens;
-  console.log("access_tokens: ", access_tokens);
 
   let accountsResponses = [];
 
@@ -235,11 +182,7 @@ app.get("/api/accounts", async function(request, response, next) {
     let access_token = access_tokens[i];
     let accounts = await getAccounts(access_token);
     let institution = await getInstitution(access_token);
-    console.log("accounts: ", accounts);
-    console.log("institution: ", institution);
-    console.log("institution name: ", institution.institution.name);
     let accountsData = { accounts, institution: institution.institution.name};
-      // prettyPrintResponse(accountsResponse);
     accountsResponses.push(accountsData);
   }
   
@@ -300,37 +243,6 @@ app.get("/api/transactions", async function(request, response, next) {
   
 });
 
-
-/*
-app.get("/api/transactions", function(request, response, next) {
-  // Pull transactions for the Item for the last 30 days
-  var startDate = moment()
-    .subtract(30, "days")
-    .format("YYYY-MM-DD");
-  var endDate = moment().format("YYYY-MM-DD");
-  client.getTransactions(
-    ACCESS_TOKEN,
-    startDate,
-    endDate,
-    {
-      count: 250,
-      offset: 0
-    },
-    function(error, transactionsResponse) {
-      if (error != null) {
-        prettyPrintResponse(error);
-        return response.json({
-          error: error
-        });
-      } else {
-        prettyPrintResponse(transactionsResponse);
-        response.json(transactionsResponse);
-      }
-    }
-  );
-});
-*/
-
 // Retrieve Identity for an Item
 // https://plaid.com/docs/#identity
 app.get("/api/identity", function(request, response, next) {
@@ -358,46 +270,6 @@ app.get("/api/balance", function(request, response, next) {
     }
     prettyPrintResponse(balanceResponse);
     response.json(balanceResponse);
-  });
-});
-
-// Retrieve Holdings for an Item
-// https://plaid.com/docs/#investments
-app.get("/api/holdings", function(request, response, next) {
-  client.getHoldings(ACCESS_TOKEN, function(error, holdingsResponse) {
-    if (error != null) {
-      prettyPrintResponse(error);
-      return response.json({
-        error: error
-      });
-    }
-    prettyPrintResponse(holdingsResponse);
-    response.json({ error: null, holdings: holdingsResponse });
-  });
-});
-
-// Retrieve Investment Transactions for an Item
-// https://plaid.com/docs/#investments
-app.get("/api/investment_transactions", function(request, response, next) {
-  var startDate = moment()
-    .subtract(30, "days")
-    .format("YYYY-MM-DD");
-  var endDate = moment().format("YYYY-MM-DD");
-  client.getInvestmentTransactions(ACCESS_TOKEN, startDate, endDate, function(
-    error,
-    investmentTransactionsResponse
-  ) {
-    if (error != null) {
-      prettyPrintResponse(error);
-      return response.json({
-        error: error
-      });
-    }
-    prettyPrintResponse(investmentTransactionsResponse);
-    response.json({
-      error: null,
-      investment_transactions: investmentTransactionsResponse
-    });
   });
 });
 
@@ -436,7 +308,7 @@ app.get("/api/item", function(request, response, next) {
 });
 
 var server = app.listen(APP_PORT, function() {
-  console.log("plaid-quickstart server listening on port " + APP_PORT);
+  console.log("Server listening on port " + APP_PORT);
 });
 
 var prettyPrintResponse = response => {
