2ce4c1f89b9db70de23bd2b3d83bec72337e01e4
Glitch (hello-express) <none>
2020-09-15 19:15:58 +0000

    🍳⛸ Checkpoint ./test/plaidClient.js:297617/705

--- test/plaidClient.js
+++ test/plaidClient.js
@@ -1,5 +1,6 @@
-var expect = require('chai').expect;
 const PlaidClient = require("../plaidClient");
+var sinon = require('sinon');
+var expect = require('chai').expect;
 
 describe("PlaidClient", function() {
   
@@ -8,9 +9,21 @@ describe("PlaidClient", function() {
     expect(plaidClient).to.not.be.null;
   });
   
-  it("should test createLinkToken", function() {
+  it("should create a createLinkToken", function() {
     const plaidClient = new PlaidClient();
+    sinon.stub(plaidClient, "createLinkToken").returns("token");
+    
     let linkToken = plaidClient.createLinkToken({});
+    expect(linkToken).to.eql("token");
+  });
+  
+  it("should exchange a public token for an access token", function() {
+    const plaidClient = new PlaidClient();
+    sinon.stub(plaidClient, "exchangePublicToken");
+    
+    let access_token = plaidClient.exchangePublicToken("public_token");
+     
+    
   });
   
 });
