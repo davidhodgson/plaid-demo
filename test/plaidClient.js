f74c37bb77584bb506d2a425c9c8dc6e2ce30892
Glitch (hello-express) <none>
2020-09-15 19:27:26 +0000

    📖🌨 Checkpoint ./test/plaidClient.js:297617/1060

--- test/plaidClient.js
+++ test/plaidClient.js
@@ -19,10 +19,31 @@ describe("PlaidClient", function() {
   
   it("should exchange a public token for an access token", function() {
     const plaidClient = new PlaidClient();
-    sinon.stub(plaidClient, "exchangePublicToken");
+    sinon.stub(plaidClient, "exchangePublicToken").returns("access_token");
     
     let access_token = plaidClient.exchangePublicToken("public_token");
-     
+    expect(access_token).to.eql("access_token"); 
+    
+  });
+  
+  it("should get account data, given an access token", function() {
+    const plaidClient = new PlaidClient();
+    sinon.stub(plaidClient, "getAccounts").returns("account_data");
+    
+    let account_data = plaidClient.getAccounts("access_token");
+    expect(account_data).to.eql("account_data");
+  });
+  
+  it("should get an institution, given an access token", function() {
+    const plaidClient = new PlaidClient();
+    sinon.stub(plaidClient, "getInstitution").returns("institution_data");
+    
+    let institution = plaidClient.getInstitution("access_token");
+    expect(institution).to.eql("institution_data");
+    
+  });
+  
+  it("should get recent transaction data, given an access token", function() {
     
   });
   
