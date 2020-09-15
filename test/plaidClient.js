da5b716e5a2f093967d838c70779a7d9fc3bd1e4
Glitch (hello-express) <none>
2020-09-15 18:53:23 +0000

    🚙🎨 Checkpoint ./package.json:297617/21 ./test/test.js:297617/156 ./test/plaidClient.js:297617/217

---
+++ test/plaidClient.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+const PlaidClient = require("./plaidClient");
+
+describe("test", function() {
+  
+  it("should run a test", function() {
+    expect(true).to.be.true;
+  })
+  
+});
---
+++ test/test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe("test", function() {
+  
+  it("should run a test", function() {
+    expect(true).to.be.true;
+  })
+  
+});
