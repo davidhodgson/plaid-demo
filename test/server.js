2c552703522f29d4de5cda3ece231bbdc3127498
Glitch (hello-express) <none>
2020-09-15 19:51:20 +0000

    🐢🕵 Checkpoint ./test/server.js:297617/275

---
+++ test/server.js
@@ -0,0 +1,16 @@
+var expect = require('chai').expect;
+const nock = require('nock');
+
+
+describe("test", function() {
+  
+  it("should run a test", function() {
+    expect(true).to.be.true;
+  });
+  
+  it("should test /api/create_link_token", function() {
+    
+    
+  });
+  
+});
